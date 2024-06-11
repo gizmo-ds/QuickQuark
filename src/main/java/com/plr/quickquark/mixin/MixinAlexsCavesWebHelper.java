7825e8027044c27fc1464600e845faa3ce1f5ac8
Gizmo <me@aika.dev>
2024-06-11 15:27:55 +0800

    feat: add support for Alex's mods

---
+++ src/main/java/com/plr/quickquark/mixin/MixinAlexsCavesWebHelper.java
@@ -0,0 +1,28 @@
+package com.plr.quickquark.mixin;
+
+import com.plr.quickquark.QuickQuark;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import javax.annotation.Nullable;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+@Mixin(com.github.alexmodguy.alexscaves.server.misc.WebHelper.class)
+public class MixinAlexsCavesWebHelper {
+    @Inject(method = "getURLContents", at = @At("HEAD"), cancellable = true, remap = false)
+    private static void inject$getURLContents(@Nullable String urlString, @Nullable String backupFileLoc, CallbackInfoReturnable<BufferedReader> cir) {
+        if (!QuickQuark.disableFetching.get()) return;
+        if (backupFileLoc != null) {
+            try {
+                cir.setReturnValue(new BufferedReader(new InputStreamReader(Objects.requireNonNull(MixinAlexsCavesWebHelper.class.getClassLoader().getResourceAsStream(backupFileLoc)), StandardCharsets.UTF_8)));
+            } catch (Exception e) {
+                cir.setReturnValue(null);
+            }
+        } else cir.setReturnValue(null);
+    }
+}
